45474c7ebd7cfdd48eb3427a64d9303bc1471a4e
tanmaykule <tanmaykule17@gmail.com>
2025-12-29 12:27:55 +0530

    Use URL instead of individual params to connect to db

--- config/postgres.go
+++ config/postgres.go
@@ -14,13 +14,14 @@ func ConnectDB() (*sql.DB, error) {
 	if err := godotenv.Load(); err != nil {
 		log.Println("no .env found")
 	}
-	connStr := fmt.Sprintf(
-		"user=%s password=%s dbname=%s sslmode=%s",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_SSLMODE"),
-	)
+	// connStr := fmt.Sprintf(
+	// 	"user=%s password=%s dbname=%s sslmode=%s",
+	// 	os.Getenv("DB_USER"),
+	// 	os.Getenv("DB_PASSWORD"),
+	// 	os.Getenv("DB_NAME"),
+	// 	os.Getenv("DB_SSLMODE"),
+	// )
+	connStr := os.Getenv("DB_URL")
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, fmt.Errorf("error opnening the DB: %w", err)
